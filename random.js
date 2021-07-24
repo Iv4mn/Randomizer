963ab8a5dfcb59146d543dd1a2fc65525c844b74
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-07-23 21:49:45 -0300

    Add files via upload

---
+++ random.js
@@ -0,0 +1,11 @@
+function random() {
+	let randomNumber = Math.floor(Math.random() * 11)
+	console.log(randomNumber)
+	let userAnswer = prompt("Dime un número del 1 al 10.")
+
+	if (userAnswer == randomNumber) {
+		alert("Le atinaste perro, sí era el " + userAnswer);
+	} 
+
+	else {alert("No le atinaste mamahuevazo, era el " + randomNumber)}
+}
