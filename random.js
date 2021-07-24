76b9b5be54fc3fd3f3ec5824a13e8d36f5aa0c84
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-07-23 22:08:30 -0300

    Update random.js

--- random.js
+++ random.js
@@ -1,7 +1,7 @@
 function random() {
 	let randomNumber = Math.floor(Math.random() * 11)
 	console.log(randomNumber)
-	let userAnswer = prompt("Dime un número del 1 al 10.")
+	let userAnswer = prompt("Dime un número del 0 al 10.")
 
 	if (userAnswer == randomNumber) {
 		alert("Le atinaste perro, sí era el " + userAnswer);
