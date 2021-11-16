4f4dec319909a7b587b3654d70e064b62ede668c
Ivamn <user.name>
2021-11-15 23:33:42 -0300

    update and I think this is the last

--- random.js
+++ random.js
@@ -2,7 +2,7 @@ function random() {
 	let randomNumber = Math.floor(Math.random() * 11)
 	let userAnswer = prompt("Dime un número del 0 al 10.")
 	if (userAnswer == randomNumber) {
-		alert("Felicidades, sí era el " + userAnswer);
+		alert(`Felicidades, sí era el ${userAnswer}`);
 	}
-	else { alert("Error, el numero correcto era el " + randomNumber) }
+	else { alert(`Error, el numero correcto era el ${randomNumber}`) }
 }
